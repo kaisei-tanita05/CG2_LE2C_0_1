fbcf0caf1ffa2b3f31a267c08dd2419b89eb3f92
kaisei-tanita05 <k024g0081@g.neec.ac.jp>
2025-04-22 10:59:03 +0900

    エラーなしver.

--- main.cpp
+++ main.cpp
@@ -8,10 +8,50 @@
 #include <d3d12.h>
 #include <dxgi1_6.h>
 #include <cassert>
+#include <DbgHelp.h>
+#include <strsafe.h>
 
 #pragma comment(lib,"d3d12.lib")
 #pragma comment(lib,"dxgi.lib")
+#pragma comment(lib,"Dbghelp.lib")
 
+
+static LONG WINAPI ExportDump(EXCEPTION_POINTERS* exception) {
+	//中身はこれから埋める
+
+	SYSTEMTIME time;
+
+	GetLocalTime(&time);
+
+	wchar_t filePath[MAX_PATH] = { 0 };
+
+	CreateDirectory(L"./Dumps", nullptr);
+
+	StringCchPrintfW(filePath, MAX_PATH, L"./Dumps/%04d-%02d%02d-%02d%02d.dmp", time.wYear, time.wMonth, time.wDay, time.wHour, time.wMinute);
+
+	HANDLE dumpFileHandle = CreateFile(filePath, GENERIC_READ | GENERIC_WRITE, FILE_SHARE_WRITE | FILE_SHARE_READ, 0, CREATE_ALWAYS, 0, 0);
+
+	//processid(このexeのId)とクラッシュ(例外)の発生したthreadIdを取得
+	DWORD processId = GetCurrentProcessId();
+	DWORD threadId = GetCurrentThreadId();
+
+	//設定情報を入力
+	MINIDUMP_EXCEPTION_INFORMATION minidumpInformation{ 0 };
+
+	minidumpInformation.ThreadId = threadId;
+
+	minidumpInformation.ExceptionPointers = exception;
+
+	minidumpInformation.ClientPointers = TRUE;
+
+	//Dumpを出力。MiniDumpNormalは最低限の情報を出力するフラグ
+	MiniDumpWriteDump(GetCurrentProcess(), processId, dumpFileHandle, MiniDumpNormal, &minidumpInformation, nullptr, nullptr);
+
+	//他に関連づけられているSEF例外ハンドラがあれば実行。通常はプロセスを終了する
+
+
+	return EXCEPTION_EXECUTE_HANDLER;
+}
 
 std::wstring ConvertString(const std::string& str) {
 	if (str.empty()) {
@@ -64,6 +104,8 @@ void Log(std::ostream& os, const std::string& message) {
 
 //Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	SetUnhandledExceptionFilter(ExportDump);
 
 	std::filesystem::create_directory("logs");
 
