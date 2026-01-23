fdf087c121d06c62a7e72fec86f011367f6eca0d
kaisei-tanita05 <k024g0081@g.neec.ac.jp>
2026-01-23 11:58:53 +0900

    a

---
+++ LE2C_20_タニタ_カイセイ/Object3d.PS.hlsl
@@ -0,0 +1,57 @@
+#include "Object3d.hlsli"
+
+ConstantBuffer<Material> gMaterial : register(b0);
+ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
+ConstantBuffer<Camera> gCamera : register(b2);
+Texture2D<float32_t4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
+
+
+struct PixelShaderOutput
+{
+    float32_t4 color : SV_TARGET0;
+};
+
+
+
+PixelShaderOutput main(VertexShaderOutput input)
+{
+    PixelShaderOutput output;
+    
+    float4 transformedUV = mul(float32_t4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransfotm);
+    
+    float32_t4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
+    
+    
+    
+    if (gMaterial.enableLighting != 0)
+    {
+        float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+        
+        float32_t3 toEye = normalize(gCamera.worldPosition - input.worldPosiiton);
+        
+        float32_t3 reflectLight = reflect(gDirectionalLight.direction, normalize(input.normal));
+        
+        float RdotE = dot(reflectLight, toEye);
+        float specularPow = pow(saturate(RdotE), gMaterial.shininess); // 反射強度
+        
+        float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
+        
+        // 拡散反射
+        float32_t3 diffuse = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
+        // 鏡面反射
+        float32_t3 specular = gDirectionalLight.color.rgb * gDirectionalLight.intensity * specularPow * float32_t3(1.0f, 1.0f, 1.0f);
+        
+        output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+        output.color.rgb = diffuse + specular;
+        output.color.a = gMaterial.color.a * textureColor.a;
+    }
+    else
+    { //Lightingしない場合。前回までと同じ演算
+        output.color = gMaterial.color * textureColor;
+    }
+    
+    return output;
+}
+
+
---
+++ LE2C_20_タニタ_カイセイ/Object3d.VS.hlsl
@@ -0,0 +1,20 @@
+#include "Object3d.hlsli"
+
+
+ConstantBuffer<TransformationMatrix> gTransformationMatrix : register(b0);
+
+//struct VertexShaderOutput
+//{
+//    float32_t4 position : SV_POSITION;
+//};
+
+VertexShaderOutput main(VertexShaderInput input)
+{
+    VertexShaderOutput output;
+    
+    output.position = mul(input.position,gTransformationMatrix.WVP);
+    output.texcoord = input.texcoord;
+    output.normal = normalize(mul(input.normal, (float32_t3x3) gTransformationMatrix.World));
+    output.worldPosiiton = mul(input.position, gTransformationMatrix.World).xyz;
+    return output;
+}
